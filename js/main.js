2e355ddf3ff61c4c4e4f47ca93187aae8c863503
Md. Abul Hasan <abul.hasan.ict@gmail.com>
2022-11-23 10:41:23 +0600

    Install Tsc and compile using TSC

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// 2 Installing Typescript Compiler in the project and system. Compile ts file using tsc.
+//console.log('Hello world');
+
+
+// 3. Compiling Typescript Code and compare the compiled Javascript code with typescript code
+
+
