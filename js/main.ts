84b5a01a1dd32e65c9fcda0b71623df98b1ecc5e
Md. Abul Hasan <abul.hasan.ict@gmail.com>
2022-11-23 17:41:07 +0600

    Type Assignment and Type Inference
    Assign type directly to variable while declaring

--- js/main.ts
+++ js/main.ts
@@ -3,7 +3,15 @@ function add(number1: number, number2: number) {
 }
 
 
- const n1 = 1.2;
+ let n1 = 20;
  const n2 = 4;
+
+ let str = 'hello';
+
+ let x:boolean;
+
+ str = 10;
+
+ 
  const result = add(n1, n2);
  console.log(result)
