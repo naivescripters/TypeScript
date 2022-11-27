1565a6c7c3939c8747ea1e2843cfb0d018b25845
Md. Abul Hasan <abul.hasan.ict@gmail.com>
2022-11-27 14:28:26 +0600

    Tuples in the typescript

--- js/main.ts
+++ js/main.ts
@@ -1,12 +1,16 @@
-let person = {
-    name: 'Leela',
-    age: 31,
-    hobbies: ['sports', 'cooking']
+let person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+    name: 'Hasan',
+    age: 27,
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
 };
 
-let favorites: string[];
-
-favorites = ['animals']; 
+person.role = [34, 'Hello'];
 
 for(let hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
