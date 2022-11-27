b601970c585587206fab93376b5f52d94d532b8d
Md. Abul Hasan <abul.hasan.ict@gmail.com>
2022-11-27 12:10:29 +0600

    Object Types in the Typescript
    Declaring the type for the object in the typescript code

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function add(number1, number2) {
-    return number1 + number2;
-}
-var n1 = 1.2;
-var n2 = 4;
-var result = add(n1, n2);
-console.log(result);
--- js/main.ts
+++ js/main.ts
@@ -1,17 +1,9 @@
-function add(number1: number, number2: number) {
-    return number1 + number2;
-}
+let person: {
+    name: string;
+    age: number;
+} = {
+    name: 'Leela',
+    age: 30
+};
 
-
- let n1 = 20;
- const n2 = 4;
-
- let str = 'hello';
-
- let x:boolean;
-
- str = 10;
-
- 
- const result = add(n1, n2);
- console.log(result)
+console.log(person.name); 
